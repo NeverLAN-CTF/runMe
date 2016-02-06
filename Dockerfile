cab2a986e619109c873d118a2f19a18806c595dc
Zane Durkin <zanedurkin@gmail.com>
2016-02-05 18:05:53 -0700

    first commit

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM richarvey/nginx-php-fpm
+MAINTAINER Zane Durkin <zane@zemptech.com>
+COPY web /usr/share/nginx/html
---
+++ web/index.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+    <head><title>Bash</title></head>
+    <body>
+        <a href="runMe.sh" download>download file</a><p>hint: in windows try using cygwin</p>
+    </body>
+</html>
